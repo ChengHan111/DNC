96240addf51ab00c9205e49c2f831552fb4588ee
ChengHan111 <tankhc1@163.com>
2022-11-18 18:21:44 -0500

    update init

--- DNC_classification/mmcls/models/heads/__init__.py
+++ DNC_classification/mmcls/models/heads/__init__.py
@@ -5,8 +5,9 @@ from .multi_label_head import MultiLabelClsHead
 from .multi_label_linear_head import MultiLabelLinearClsHead
 from .stacked_head import StackedLinearClsHead
 from .vision_transformer_head import VisionTransformerClsHead
+from .SubCentroids_head_Formal import SubCentroids_Head_Formal
 
 __all__ = [
     'ClsHead', 'LinearClsHead', 'StackedLinearClsHead', 'MultiLabelClsHead',
-    'MultiLabelLinearClsHead', 'VisionTransformerClsHead'
+    'MultiLabelLinearClsHead', 'VisionTransformerClsHead', 'SubCentroids_Head_Formal'
 ]
